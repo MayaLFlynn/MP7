69e9d81c4e8be83de4045daf2f3f6d897d337364
Maya Flynn <flynnmay@grinnell.edu>
2024-04-10 16:30:55 -0500

    Added comments to Block

--- src/Block.java
+++ src/Block.java
@@ -3,6 +3,15 @@ import java.security.NoSuchAlgorithmException;
 import java.nio.ByteBuffer;
 import java.util.Random;
 
+
+/**
+ * Block is an object with a number that represents its order in a chain (though that is not
+ * enforced), an amount that represent a numerical transaction, a previous Hash , a Hash, and a
+ * nonce that together with other blocks helps the user verify if the block is valid.
+ * 
+ * @author Alyssa Trapp
+ * @author Maya Flynn
+ */
 public class Block {
 
   // FIELDS
@@ -17,15 +26,15 @@ public class Block {
     this.num = num;
     this.amount = amount;
     this.prevHash = prevHash;
-    // loops while it is true 
+    // loops while it is true
     while (true) {
       try {
         // creates a rand type object
         Random rand = new Random();
         // generates a random long
-        long randNonce = rand.nextLong();
+        long randNonce = rand.nextInt();
         // generates a random hash based on the random nonce
-        Hash tempHash = calculateHash(randNonce);  
+        Hash tempHash = calculateHash(randNonce);
         // if it's a valid hash, set nonce to randNonce and curHash to tempHash
         // if not, keep looping until you do get a valid hash
         if (tempHash.isValid()) {
@@ -34,8 +43,8 @@ public class Block {
           return;
         } // if
       } catch (NoSuchAlgorithmException e) {
-      } // catch 
-    } // while 
+      } // catch
+    } // while
   } // Block (int, int, Hash)
 
   Block(int num, int amount, Hash prevHash, long nonce) {
@@ -47,13 +56,12 @@ public class Block {
     try {
       this.curHash = calculateHash(nonce);
     } catch (NoSuchAlgorithmException e) {
-
     } // catch
 
   } // Block(int, int, Hash, long)
 
   /*
-   * Calculates the hash  
+   * Calculates the hash
    */
   public Hash calculateHash(long nonce) throws NoSuchAlgorithmException {
     // creates a message digest object
@@ -77,48 +85,59 @@ public class Block {
     return new Hash(md.digest());
   } // calculateHash(String)
 
+  /**
+   * Gets the number of the block, will be the index of the block in a BlockChain
+   * 
+   * @return
+   */
   public int getNum() {
     return this.num;
   } // getNum()
 
+  /**
+   * Gets the amount of the block, will be the amount of 'money' transfered in the block
+   * 
+   * @return
+   */
   public int getAmount() {
     return this.amount;
   } // getAmount()
 
+  /**
+   * Gets the nonce, the number to calculate the hash with
+   * 
+   * @return
+   */
   public long getNonce() {
     return this.nonce;
   } // getNonce
 
+  /**
+   * Gets the previous Hash, will make sure that there were no elements added in the middle of the
+   * chain
+   * 
+   * @return
+   */
   public Hash getPrevHash() {
     return this.prevHash;
   } // getPrevHash()
 
+  /**
+   * Gets the current Hash, allows the user to control valid nonces and Hashes and track if the
+   * following element is correct
+   * 
+   * @return
+   */
   public Hash getHash() {
     return this.curHash;
   } // getHash()
 
-  public String toString() {
-    return "Block " + num + " (Amount: " + amount + ", Nonce: " + nonce + ", prevHash: " + prevHash + ", hash: "
-        + curHash + ")";
-  } // toString()
-
-  // this might go to experiments
-  /*
-   * long count = 0;
-   * long startTime = System.currentTimeMillis();
-   * Hash hash;
-   * long nonce;
-   * do {
-   * nonce = rand.nextLong();
-   * hash = computeHash(num, amount, prevHash, nonce);
-   * ++count;
-   * if (VERBOSE && (0 == (count % 100000))) {
-   * System.err.printf("Generated %d nonces in %d milliseconds.\n",
-   * count,
-   * System.currentTimeMillis() - startTime);
-   * }
-   * } while (! hash.isValid());
-   * /*
+  /**
+   * Turns the Block into a string
    */
+  public String toString() {
+    return "Block " + num + " (Amount: " + amount + ", Nonce: " + nonce + ", prevHash: " + prevHash
+        + ", hash: " + curHash + ")";
+  } // toString()
 
 } // class Block
